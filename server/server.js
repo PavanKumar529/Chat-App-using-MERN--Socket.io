d2af01baf08880601be521c82361e429fe6233b4
Pavan Kumar Kuruva <kpavankumark9783@gmail.com>
2024-07-29 19:28:48 +0530

    Add .env file to manage environment variables securely.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const app = express()
 
+const hostName = process.env.HOST_NAME
+const PORT = process.env.PORT
 
 // Demo API
 app.get("/", (req, res) => {
@@ -9,6 +14,6 @@ app.get("/", (req, res) => {
 })
 
 app.listen(5000, () => {
-    console.log("Server Running on PORT 5000");
+    console.log(`Server running at http://${hostName}:${PORT}`);
 })
 
